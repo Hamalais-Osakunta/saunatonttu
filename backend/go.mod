1193c0382fd16d0b5a72ab914db52224c6f5b6c6
Niilo Kurki <niilo.kurki@helsinki.fi>
2024-10-11 15:49:44 +0300

    Fixed notification bug and added tests

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,7 @@ module bt-telegram
 go 1.23.0
 
 require (
-	github.com/go-telegram/bot v1.7.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/peterhellberg/ruuvitag v0.1.0 // indirect
+	github.com/go-telegram/bot v1.7.2
+	github.com/joho/godotenv v1.5.1
+	github.com/peterhellberg/ruuvitag v0.1.0
 )
--- backend/main.go
+++ backend/main.go
@@ -12,13 +12,164 @@ import (
 	"time"
 
 	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 	"github.com/peterhellberg/ruuvitag"
 )
 
-var saunaKiuas  = Kiuas{
-	TemperatureRecords:  [3]float64{0.0, 0.0, 0.0},
-	TimestampRecords:    [3]time.Time{time.Now(), time.Now(), time.Now()},
+import _ "time/tzdata"
+
+type Kiuas struct {
+	Temperature             float64
+	Humidity                float64
+	Battery                 uint16
+	WarmingNotificationSent bool
+	ReadyNotificationSent   bool
+	LastDataReceived        time.Time
+	TemperatureRecords      [3]float64
+	TimestampRecords        [3]time.Time
+}
+
+func (k *Kiuas) IsOn() bool {
+	return k.Temperature > 0
+}
+
+// Shift the old values and add a new temperature record
+func (k *Kiuas) AddTemperatureRecord(newTemp float64, newTime time.Time) {
+	// Shift the array to remove the oldest record and add the new one
+	k.TemperatureRecords[0] = k.TemperatureRecords[1]
+	k.TemperatureRecords[1] = k.TemperatureRecords[2]
+	k.TemperatureRecords[2] = newTemp
+
+	// Similarly update the timestamps
+	k.TimestampRecords[0] = k.TimestampRecords[1]
+	k.TimestampRecords[1] = k.TimestampRecords[2]
+	k.TimestampRecords[2] = newTime
+}
+
+func GetSaunaStatus(isOn bool) string {
+	if isOn {
+		return "päällä"
+	}
+	return "pois päältä"
+}
+
+func (k *Kiuas) ResetNotifications() {
+	k.WarmingNotificationSent = false
+	k.ReadyNotificationSent = false
+}
+
+type TelegramBot interface {
+	SendMessage(ctx context.Context, params *bot.SendMessageParams) (*models.Message, error)
+	RegisterHandler(handlerType bot.HandlerType, pattern string, matchType bot.MatchType, handler bot.HandlerFunc)
+	Start(ctx context.Context)
+	SetMyCommands(ctx context.Context, params *bot.SetMyCommandsParams) error
+}
+
+type BotWrapper struct {
+	Bot *bot.Bot
+}
+
+func (b *BotWrapper) SendMessage(ctx context.Context, params *bot.SendMessageParams) (*models.Message, error) {
+	return b.Bot.SendMessage(ctx, params)
+}
+
+func (b *BotWrapper) RegisterHandler(handlerType bot.HandlerType, pattern string, matchType bot.MatchType, handler bot.HandlerFunc) {
+	b.Bot.RegisterHandler(handlerType, pattern, matchType, handler)
+}
+
+func (b *BotWrapper) Start(ctx context.Context) {
+	b.Bot.Start(ctx)
+}
+
+func (b *BotWrapper) SetMyCommands(ctx context.Context, params *bot.SetMyCommandsParams) error {
+	_, err := b.Bot.SetMyCommands(ctx, params)
+	return err
+}
+
+type Config struct {
+	ReadyThreshold     float64
+	ChangeThreshold    float64
+	LowerBound         float64
+	ResetThreshold     float64
+	MaintenanceChatID  int64
+	NotificationChatID int64
+	ServerPort         string
+	TelegramBotToken   string
+}
+
+func InitializeTelegramBot(ctx context.Context, token string, kiuas *Kiuas, config *Config) (TelegramBot, error) {
+	opts := []bot.Option{}
+
+	botInstance, err := bot.New(token, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	botWrapper := &BotWrapper{Bot: botInstance}
+
+	botWrapper.RegisterHandler(bot.HandlerTypeMessageText, "/kiuas", bot.MatchTypePrefix, func(ctx context.Context, _ *bot.Bot, update *models.Update) {
+		_, err := botWrapper.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   fmt.Sprintf("Sauna on %s\nLämpötila: %.1f °C\nKosteus: %.1f%%", GetSaunaStatus(kiuas.IsOn()), kiuas.Temperature, kiuas.Humidity),
+		})
+		if err != nil {
+			fmt.Printf("Failed to send message: %v\n", err)
+		}
+	})
+
+	botWrapper.RegisterHandler(bot.HandlerTypeMessageText, "/info", bot.MatchTypePrefix, func(ctx context.Context, _ *bot.Bot, update *models.Update) {
+		if update.Message.Chat.ID == config.MaintenanceChatID {
+			loc, err := time.LoadLocation("Europe/Bucharest")
+			if err != nil {
+				fmt.Printf("Error loading location: %v", err)
+			}
+			_, err = botWrapper.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: update.Message.Chat.ID,
+				Text: fmt.Sprintf(
+					"Sauna Info:\nTemperature: %.1f °C\nHumidity: %.1f%%\nBattery: %d V\nLast Data Received: %s",
+					kiuas.Temperature,
+					kiuas.Humidity,
+					kiuas.Battery,
+					kiuas.LastDataReceived.In(loc))})
+			if err != nil {
+				fmt.Printf("Failed to send message: %v\n", err)
+			}
+		}
+	})
+
+	err = botWrapper.SetMyCommands(ctx, &bot.SetMyCommandsParams{
+		Commands: []models.BotCommand{
+			{
+				Command:     "kiuas",
+				Description: "Näytä saunan tila",
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return botWrapper, nil
+}
+
+func SendTelegramMessage(b TelegramBot, ctx context.Context, config *Config, message string, chatID ...int64) {
+	var targetChatID int64
+
+	if len(chatID) > 0 {
+		targetChatID = chatID[0]
+	} else {
+		targetChatID = config.NotificationChatID
+	}
+
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    targetChatID,
+		Text:      message,
+		ParseMode: "MarkdownV2",
+	})
+	if err != nil {
+		fmt.Printf("Failed to send message: %v\n", err)
+	}
 }
 
 func main() {
@@ -34,63 +185,99 @@ func main() {
 	if botToken == "" {
 		log.Fatalf("TELEGRAM_BOT_TOKEN is not set in the environment")
 	}
-	b, err := InitializeTelegramBot(ctx, botToken, &saunaKiuas)
+
+	readyThreshold, err := strconv.ParseFloat(os.Getenv("SAUNA_READY_THRESHOLD"), 64)
 	if err != nil {
-		log.Fatalf("Failed to initialize Telegram bot: %v", err)
+		log.Fatalf("Error parsing SAUNA_READY_THRESHOLD: %v", err)
 	}
 
-	go b.Start(ctx)
+	maintenanceChatID, err := strconv.ParseInt(os.Getenv("MAINTENANCE_CHAT_ID"), 10, 64)
+	if err != nil {
+		log.Fatalf("Error parsing MAINTENANCE_CHAT_ID: %v", err)
+	}
 
-	go startHTTPServer(b, ctx)
-
-	go monitorDataReception(b, ctx)
-
-	<-ctx.Done()
-	fmt.Println("Shutting down...")
-}
-
-func startHTTPServer(b *bot.Bot, ctx context.Context) {
-	http.HandleFunc("/api/receive-bt", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-			return
-		}
-
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-			return
-		}
-		defer r.Body.Close()
-
-		ruuviTag, err := ruuvitag.ParseRAWv2(body)
-		if err != nil {
-			fmt.Println("Failed to parse RuuviTag data. are all the sensors enabled?", err)
-		}
-
-		saunaKiuas.Temperature = ruuviTag.Temperature
-		saunaKiuas.Humidity = ruuviTag.Humidity
-		saunaKiuas.Battery = ruuviTag.Battery
-		fmt.Printf("Received new temperature value: %.1f °C, Humidity: %.1f%%, Voltage: %d V\n", saunaKiuas.Temperature, saunaKiuas.Humidity, saunaKiuas.Battery)
-
-		saunaKiuas.lastDataReceived = time.Now()
-		// Update the temperature records with the latest temperature and timestamp
-		saunaKiuas.AddTemperatureRecord(saunaKiuas.Temperature, time.Now())
-
-		checkAndNotify(b, ctx)
-	})
+	notificationChatID, err := strconv.ParseInt(os.Getenv("NOTIFICATION_CHAT_ID"), 10, 64)
+	if err != nil {
+		log.Fatalf("Error parsing NOTIFICATION_CHAT_ID: %v", err)
+	}
 
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
 		port = "1337"
 	}
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	config := &Config{
+		ReadyThreshold:     readyThreshold,
+		ChangeThreshold:    0.0123,
+		LowerBound:         0.0123 * 0.9,
+		ResetThreshold:     40.0,
+		MaintenanceChatID:  maintenanceChatID,
+		NotificationChatID: notificationChatID,
+		ServerPort:         port,
+		TelegramBotToken:   botToken,
+	}
+
+	kiuas := &Kiuas{
+		TemperatureRecords: [3]float64{0.0, 0.0, 0.0},
+		TimestampRecords:   [3]time.Time{time.Now(), time.Now(), time.Now()},
+	}
+
+	botInstance, err := InitializeTelegramBot(ctx, botToken, kiuas, config)
+	if err != nil {
+		log.Fatalf("Failed to initialize Telegram bot: %v", err)
+	}
+
+	go botInstance.Start(ctx)
+
+	go startHTTPServer(botInstance, ctx, kiuas, config)
+
+	go monitorDataReception(botInstance, ctx, kiuas, config)
+
+	<-ctx.Done()
+	fmt.Println("Shutting down...")
+}
+
+func startHTTPServer(b TelegramBot, ctx context.Context, kiuas *Kiuas, config *Config) {
+	http.HandleFunc("/api/receive-bt", func(w http.ResponseWriter, r *http.Request) {
+		handleReceiveBT(w, r, b, ctx, kiuas, config)
+	})
+
+	if err := http.ListenAndServe(":"+config.ServerPort, nil); err != nil {
 		log.Fatalf("Failed to start HTTP server: %v", err)
 	}
 }
 
-func monitorDataReception(b *bot.Bot, ctx context.Context) {
+func handleReceiveBT(w http.ResponseWriter, r *http.Request, b TelegramBot, ctx context.Context, kiuas *Kiuas, config *Config) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	ruuviTag, err := ruuvitag.ParseRAWv2(body)
+	if err != nil {
+		fmt.Println("Failed to parse RuuviTag data. Are all the sensors enabled?", err)
+		return
+	}
+
+	kiuas.Temperature = ruuviTag.Temperature
+	kiuas.Humidity = ruuviTag.Humidity
+	kiuas.Battery = ruuviTag.Battery
+	fmt.Printf("Received new temperature value: %.1f °C, Humidity: %.1f%%, Voltage: %d V\n", kiuas.Temperature, kiuas.Humidity, kiuas.Battery)
+
+	kiuas.LastDataReceived = time.Now()
+	kiuas.AddTemperatureRecord(kiuas.Temperature, time.Now())
+
+	checkAndNotify(b, ctx, kiuas, config)
+}
+
+func monitorDataReception(b TelegramBot, ctx context.Context, kiuas *Kiuas, config *Config) {
 	ticker := time.NewTicker(1 * time.Minute)
 	defer ticker.Stop()
 
@@ -99,14 +286,10 @@ func monitorDataReception(b *bot.Bot, ctx context.Context) {
 	for {
 		select {
 		case <-ticker.C:
-			if time.Since(saunaKiuas.lastDataReceived) > time.Hour && !notificationSent {
-				maintenanceChatID, err := strconv.ParseInt(os.Getenv("MAINTENANCE_CHAT_ID"), 10, 64)
-				if err != nil {
-					log.Fatalf("Error parsing MAINTENANCE_CHAT_ID: %v", err)
-				}
-				SendTelegramMessage(b, ctx, "No data received for over 1 hour", maintenanceChatID)
+			if time.Since(kiuas.LastDataReceived) > time.Hour && !notificationSent {
+				SendTelegramMessage(b, ctx, config, "No data received for over 1 hour", config.MaintenanceChatID)
 				notificationSent = true
-			} else if time.Since(saunaKiuas.lastDataReceived) <= time.Hour {
+			} else if time.Since(kiuas.LastDataReceived) <= time.Hour {
 				notificationSent = false
 			}
 		case <-ctx.Done():
@@ -116,48 +299,41 @@ func monitorDataReception(b *bot.Bot, ctx context.Context) {
 }
 
 // Function to check temperature change and send notifications
-func checkAndNotify(b *bot.Bot, ctx context.Context) {
-	readyThreshold, err := strconv.ParseFloat(os.Getenv("SAUNA_READY_THRESHOLD"), 64)
-	if err != nil {
-		log.Fatalf("Error parsing SAUNA_READY_THRESHOLD: %v", err)
-	}
+func checkAndNotify(b TelegramBot, ctx context.Context, kiuas *Kiuas, config *Config) {
+    // Ensure there are at least three valid records
+    if kiuas.TemperatureRecords[0] == 0 {
+        log.Println("Not enough valid timestamp records")
+        return
+    }
 
-	// Ensure there are at least three valid records
-	if saunaKiuas.TemperatureRecords[0] == 0 {
-		log.Println("Not enough valid timestamp records")
-		return
-	}
+    // Calculate the time difference and average temperature change over the last three records
+    timeDiff := kiuas.TimestampRecords[2].Sub(kiuas.TimestampRecords[0]).Seconds()
+    if timeDiff == 0 {
+        // Avoid division by zero if the timestamps are identical (unlikely but possible)
+        return
+    }
 
-	// Calculate the time difference and average temperature change over the last three records
-	timeDiff := saunaKiuas.TimestampRecords[2].Sub(saunaKiuas.TimestampRecords[0]).Seconds()
-	if timeDiff == 0 {
-		// Avoid division by zero if the timestamps are identical (unlikely but possible)
-		return
-	}
+    // Calculate the temperature change over the three records
+    tempChange := kiuas.TemperatureRecords[2] - kiuas.TemperatureRecords[0]
+    tempChangeRate := tempChange / timeDiff
 
-	// Calculate the temperature change over the three records
-	tempChange := saunaKiuas.TemperatureRecords[2] - saunaKiuas.TemperatureRecords[0]
-	tempChangeRate := tempChange / timeDiff
+    // Ready notification check
+    if kiuas.Temperature >= config.ReadyThreshold {
+        if !kiuas.ReadyNotificationSent {
+            SendTelegramMessage(b, ctx, config, fmt.Sprintf("*Sauna valmis\\!*🔥\nLämpötila: %.1f °C 🌡️", kiuas.Temperature))
+            kiuas.ReadyNotificationSent = true
+        }
+    } else if !kiuas.WarmingNotificationSent && !kiuas.ReadyNotificationSent {
+        if tempChangeRate >= config.LowerBound {
+            SendTelegramMessage(b, ctx, config, "🔥*Sauna lämpiää\\!*🔥")
+            kiuas.WarmingNotificationSent = true
+        }
+    }
 
-	// Threshold for change rate considered as sauna warming up
-	changeThreshold := 0.0123 // avg temperature change / second
-
-	// Introduce lower bound for the change rate to catch the case where the temperature is rising slowly
-	lowerBound := changeThreshold * 0.9
-
-	// Ready notification check
-	if saunaKiuas.Temperature >= readyThreshold {
-		if !saunaKiuas.ReadyNotificationSent {
-			SendTelegramMessage(b, ctx, fmt.Sprintf("*Sauna valmis\\!*🔥\nLämpötila: %.1f °C 🌡️", saunaKiuas.Temperature))
-			saunaKiuas.ReadyNotificationSent = true
-		}
-	} else if !saunaKiuas.WarmingNotificationSent && !saunaKiuas.ReadyNotificationSent {
-			if tempChangeRate >= lowerBound {
-				SendTelegramMessage(b, ctx, "🔥*Sauna lämpiää\\!*🔥")
-				saunaKiuas.WarmingNotificationSent = true
-			}
-	} else {
-		// Reset notifications if temperature drops below warming threshold
-		saunaKiuas.ResetNotifications()
-	}
+    // Reset notifications if temperature has cooled down
+    if kiuas.Temperature < config.ResetThreshold {
+        if kiuas.WarmingNotificationSent || kiuas.ReadyNotificationSent {
+            kiuas.ResetNotifications()
+        }
+    }
 }
